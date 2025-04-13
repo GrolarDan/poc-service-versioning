dd5b05080ea92b7a4dafa8578c928891f3e384b0
Daniel Masek <daniel@masci.cz>
2025-04-13 22:57:45 +0200

    First commit

---
+++ publishing-house-service/src/main/java/dmk/poc/publishinghouseservice/PublishingHouseApplication.java
@@ -0,0 +1,13 @@
+package dmk.poc.publishinghouseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishingHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishingHouseApplication.class, args);
+	}
+
+}
