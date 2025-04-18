febc6d7ec0676b6ed990a7ad5acd16aacccd9df2
Daniel Masek <daniel@masci.cz>
2025-04-18 20:28:02 +0200

    Add web application for first book store

---
+++ all-you-can-read/src/main/java/dmk/poc/allyoucanread/AllYouCanReadApplication.java
@@ -0,0 +1,12 @@
+package dmk.poc.allyoucanread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllYouCanReadApplication  {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AllYouCanReadApplication.class, args);
+  }
+}
--- publishing-house-service/src/test/java/dmk/poc/serviceversioning/PublishingHouseApplicationTests.java
+++ publishing-house-service/src/test/java/dmk/poc/serviceversioning/PublishingHouseApplicationTests.java
@@ -1,9 +1,12 @@
 package dmk.poc.serviceversioning;
 
+import dmk.poc.publishinghouseservice.PublishingHouseApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		classes = PublishingHouseApplication.class,
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PublishingHouseApplicationTests {
 
 	@Test
